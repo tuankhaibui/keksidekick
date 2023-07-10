7600124b9adc98e0e2d94dd75821694c9754fc79
tuankhaibui <tuankhaibui.physics@gmail.com>
2023-07-10 11:30:04 +0900

    update keksetup.sh

--- keksetup.sh
+++ keksetup.sh
@@ -11,14 +11,27 @@ function PrepareScript () {
     echo "#!/bin/bash"
     echo "cd \$HOME"
     echo "echo 'this script to setup python environment in KEK CC'"
+    echo "echo 'the python environment $1 will be setup via pyenv'"
     echo "echo 'script created: $(date)'"
 
+    echo "# Clone pyenv repository & set up python-$1"
     echo "git clone https://github.com/pyenv/pyenv.git ~/.pyenv"
     echo "echo 'export PYENV_ROOT=\"\$HOME/.pyenv\"' >> ~/.bashrc"
     echo "echo 'export PATH=\"\$PYENV_ROOT/bin:\$PATH\"' >> ~/.bashrc"
     echo "echo -e 'if command -v pyenv 1>/dev/null 2>&1; then\n  eval \"\$(pyenv init -)\"\\nfi' >> ~/.bashrc"
     echo "pyenv install $1"
     echo "pyenv global $1"
+    echo "echo 'The current python version is: ' \$(python --version) "
+    echo "echo '                   path is   : ' \$(which python)"
+
+
+    echo "# Create a virtual environment based on python-$1"
+    echo "python -m venv ~/.venvs/py$1"
+    echo "source ~/.venvs/py$1/bin/activate"
+    echo "# Install pip if needed"
+    echo -e "if [ -z $(which pip) ]; then\n\tpython -m ensurepip --upgrade\nfi"
+    echo "pip install --upgrade pip"
+
     
 }
 
