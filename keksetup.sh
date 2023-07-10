dad9b4302dad6f2911f2bf4a4ebfcf4bcf7bc1ee
tuankhaibui <tuankhaibui.physics@gmail.com>
2023-07-10 12:59:39 +0900

    update script

--- keksetup.sh
+++ keksetup.sh
@@ -14,11 +14,16 @@ function PrepareScript () {
     echo "echo 'the python environment $1 will be setup via pyenv'"
     echo "echo 'script created: $(date)'"
 
+    echo "# Prepare a GCC/G++ 7.5.0 environment"
+    echo "module load gcc/750"
+    echo "export CC='/opt/gcc-7.5.0/bin/gcc'"
+    echo "export CXX='/opt/gcc-7.5.0/bin/g++'"
+
     echo "# Clone pyenv repository & set up python-$1"
-    echo "git clone https://github.com/pyenv/pyenv.git ~/.pyenv"
-    echo "echo 'export PYENV_ROOT=\"\$HOME/.pyenv\"' >> ~/.bashrc"
-    echo "echo 'export PATH=\"\$PYENV_ROOT/bin:\$PATH\"' >> ~/.bashrc"
-    echo "echo -e 'if command -v pyenv 1>/dev/null 2>&1; then\n  eval \"\$(pyenv init -)\"\\nfi' >> ~/.bashrc"
+    echo "git clone https://github.com/pyenv/pyenv.git \$HOME/.pyenv"
+    echo "echo 'export PYENV_ROOT=\"\$HOME/.pyenv\"' >> \$HOME/.bashrc"
+    echo "echo 'export PATH=\"\$PYENV_ROOT/bin:\$PATH\"' >> \$HOME/.bashrc"
+    echo "echo -e 'if command -v pyenv 1>/dev/null 2>&1; then\n  eval \"\$(pyenv init -)\"\\nfi' >> \$HOME/.bashrc"
     echo "pyenv install $1"
     echo "pyenv global $1"
     echo "echo 'The current python version is: ' \$(python --version) "
@@ -26,14 +31,14 @@ function PrepareScript () {
 
 
     echo "# Create a virtual environment based on python-$1"
-    echo "python -m venv ~/.venvs/py$1"
-    echo "source ~/.venvs/py$1/bin/activate"
+    echo "python -m venv \$HOME/.venvs/py$1"
+    echo "source \$HOME/.venvs/py$1/bin/activate"
     echo "# Install pip if needed"
-    echo -e "if [ -z $(which pip) ]; then\n\tpython -m ensurepip --upgrade\nfi"
+    echo -e "if [ -z \$(which pip) ]; then\\n\\tpython -m ensurepip --upgrade\\nfi"
     echo "pip install --upgrade pip"
 
     echo "# Install requirements packages for further analysis"
-    echo "pip install -r ./py_requirements.txt"
+    echo "pip install -r \$HOME/keksidekick/py_requirements.txt"
 }
 
 function PrintUsage () {
@@ -43,15 +48,8 @@ function PrintUsage () {
 }
 
 ######### Main Program #########
-
-kekuser=$1 #username on KEK CC
-pyversion=$2 #desired python version
-hostname=login.cc.kek.jp
-
-if [ -z $kekuser ]; then
-    echo "no input for username-on-kek. Exit now"
-    exit 0
-fi
+kekuser=$USER
+pyversion=$1 #desired python version
 
 if [ -z $pyversion ]; then
     echo "no input for python version. Exit now"
@@ -60,15 +58,6 @@ fi
 
 script=./.python_init.sh
 PrepareScript $pyversion > $script
-scp $script ${kekuser}@${hostname}:~
-scp py_requirements.txt ${kekuser}@${hostname}:~/.py_requirements.txt
-cat $script
-
-: '
-ssh ${kekuser}@${hostname} <<EOF
-    ./$script
-    exit
-EOF
-'	
-
+#cat $script
+sh $script
 ################################
